3d08c7ba3217c0ff47421f5d351acb629fdb2bd7
Lukas Niemeyer <lniemeyer7@googlemail.com>
2022-08-26 12:49:27 +0200

    Drop allout Sout

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,23 @@
+import java.util.SortedSet;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
+        System.out.println("Hallo");
+        System.out.println("Hallo");
+
+        System.out.println("Hallo");
+        System.out.println();
+        System.out.println("Hallo");
+        System.out.println("Hallo");
+        System.out.println("Hallo");
+
+        System.out.println("Hallo");
+        System.out.println("Hallo");
+        System.out.println("Hallo");
+
+
+
 
     }
 }
